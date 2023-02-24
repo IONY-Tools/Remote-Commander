1ff3710ec74c0f4701965d1fa9f231e3a311c774
DominicTheD3v <119626487+DominicTheD3v@users.noreply.github.com>
2023-02-24 14:39:58 +0000

    Fixed

--- src/client/utils/database.py
+++ src/client/utils/database.py
@@ -1,11 +1,4 @@
 import sqlite3
 
-class DBConnection:
-    def __init__(self, filename):
-        self.db = sqlite3.connect(filename)
-
-    def exec(self, sql, commit=False):
-        result = self.db.cursor.execute(sql)
-        if commit == True:
-            self.db.commit()
-        return result
+def DBConnection(filename):
+    return sqlite3.connect(filename)
